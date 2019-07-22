46d4588d485b0e597b1136ba3b19f8b3bd348fdd
maomaomaoxiaoliu <1617643420@qq.com>
2019-07-22 16:46:52 +0800

    test

---
+++ .next/static/webpack/static/development/pages/_app.js.bd6f5a2a881a667f141e.hot-update.js
@@ -0,0 +1,389 @@
+webpackHotUpdate("static/development/pages/_app.js",{
+
+/***/ "./pages/_app.js":
+/*!***********************!*\
+  !*** ./pages/_app.js ***!
+  \***********************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _components_nav__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../components/nav */ "./components/nav.js");
+var _jsxFileName = "/home/mao/tailwindcss/my-app/pages/_app.js";
+
+
+/* harmony default export */ __webpack_exports__["default"] = (function () {
+  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    style: {
+      marginBottom: '20rem'
+    },
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 4
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_components_nav__WEBPACK_IMPORTED_MODULE_1__["default"], {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 5
+    },
+    __self: this
+  }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "md:flex",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 7
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "md:flex-shrink-0",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 8
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("img", {
+    className: "rounded-lg md:w-56",
+    src: "/static/logo.png",
+    alt: "Woman paying for a purchase",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 9
+    },
+    __self: this
+  })), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "mt-4 md:mt-0 md:ml-6",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 11
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "uppercase tracking-wide text-sm text-indigo-600 font-bold",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 12
+    },
+    __self: this
+  }, "Marketing"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("a", {
+    href: "#",
+    className: "block mt-1 text-lg leading-tight font-semibold text-gray-900 hover:underline",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 13
+    },
+    __self: this
+  }, "Finding customers for your new business"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+    className: "mt-2 text-gray-600",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 14
+    },
+    __self: this
+  }, "Getting a new business off the ground is a lot of hard work. Here are five ideas you can use to find your first customers."))), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("button", {
+    className: "btn btn-blue",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 18
+    },
+    __self: this
+  }, "Button"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("button", {
+    className: "btn btn-gray",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 22
+    },
+    __self: this
+  }, "Button"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "bg-gradient-brand",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 26
+    },
+    __self: this
+  }, "asfffffffffff"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("br", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 30
+    },
+    __self: this
+  }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("input", {
+    placeholder: "test",
+    className: "border-black focus:bg-white hover:bg-black focus:border-white hover:border-black",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 32
+    },
+    __self: this
+  }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("ul", {
+    className: "list-disc list-inside",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 34
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("li", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 35
+    },
+    __self: this
+  }, "One"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("li", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 36
+    },
+    __self: this
+  }, "Two"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("li", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 37
+    },
+    __self: this
+  }, "Three"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("li", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 38
+    },
+    __self: this
+  }, "Four")), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "bg-gray-200 p-4",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 41
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "block text-gray-700 text-center bg-gray-400 px-4 py-2",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 42
+    },
+    __self: this
+  }, "1"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "block text-gray-700 text-center bg-gray-400 px-4 py-2 mt-4",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 43
+    },
+    __self: this
+  }, "2"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "block text-gray-700 text-center bg-gray-400 px-4 py-2 mt-4",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 44
+    },
+    __self: this
+  }, "3")), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "bg-gray-200 m-5",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 47
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "inline-block text-gray-700 text-center bg-gray-400 px-4 py-2 m-2",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 48
+    },
+    __self: this
+  }, "1"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "inline-block text-gray-700 text-center bg-gray-400 px-4 py-2 m-2",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 49
+    },
+    __self: this
+  }, "2"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "inline-block text-gray-700 text-center bg-gray-400 px-4 py-2 m-2",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 50
+    },
+    __self: this
+  }, "3")), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "bg-gray-200 m-5",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 53
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "inline text-gray-700 text-center bg-gray-400 px-4 py-2 mx-2",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 54
+    },
+    __self: this
+  }, "1"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "inline text-gray-700 text-center bg-gray-400 px-4 py-2 mx-2",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 55
+    },
+    __self: this
+  }, "2"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "inline text-gray-700 text-center bg-gray-400 px-4 py-2 mx-2",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 56
+    },
+    __self: this
+  }, "3")), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "flex bg-gray-200 m-5",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 59
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "flex-1 text-gray-700 text-center bg-gray-400 px-4 py-2 m-2",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 60
+    },
+    __self: this
+  }, "1"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "flex-1 text-gray-700 text-center bg-gray-400 px-4 py-2 m-2",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 61
+    },
+    __self: this
+  }, "2"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "flex-1 text-gray-700 text-center bg-gray-400 px-4 py-2 m-2",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 62
+    },
+    __self: this
+  }, "3")), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "inline-flex bg-gray-200 m-5",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 65
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "flex-1 text-gray-700 text-center bg-gray-400 px-4 py-2 m-2",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 66
+    },
+    __self: this
+  }, "1"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "flex-1 text-gray-700 text-center bg-gray-400 px-4 py-2 m-2",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 67
+    },
+    __self: this
+  }, "2"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "flex-1 text-gray-700 text-center bg-gray-400 px-4 py-2 m-2",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 68
+    },
+    __self: this
+  }, "3")), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "table w-full",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 71
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "table-row",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 72
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "table-cell bg-gray-400 text-gray-700 px-4 py-2 text-sm",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 73
+    },
+    __self: this
+  }, "A cell with more content"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "table-cell bg-gray-200 text-gray-700 px-4 py-2 text-sm",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 74
+    },
+    __self: this
+  }, "Cell 2"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "table-cell bg-gray-400 text-gray-700 px-4 py-2 text-sm",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 75
+    },
+    __self: this
+  }, "Cell 3")), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "table-row",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 78
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "table-cell bg-gray-200 text-gray-700 px-4 py-2 text-sm",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 79
+    },
+    __self: this
+  }, "Cell 4"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "table-cell bg-gray-400 text-gray-700 px-4 py-2 text-sm",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 80
+    },
+    __self: this
+  }, "A cell with more content"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "table-cell bg-gray-200 text-gray-700 px-4 py-2 text-sm",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 81
+    },
+    __self: this
+  }, "Cell 6"))), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "flex sm:inline-block md:block lg:hidden sl:flex bg-gray-200 m-5",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 85
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "flex-1 text-gray-700 text-center bg-gray-400 px-4 py-2 m-2",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 86
+    },
+    __self: this
+  }, "1"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "flex-1 text-gray-700 text-center bg-gray-400 px-4 py-2 m-2",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 87
+    },
+    __self: this
+  }, "2"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "flex-1 text-gray-700 text-center bg-gray-400 px-4 py-2 m-2",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 88
+    },
+    __self: this
+  }, "3")));
+});
+
+/***/ })
+
+})
+//# sourceMappingURL=_app.js.bd6f5a2a881a667f141e.hot-update.js.map
---
+++ pages/_app.js
@@ -0,0 +1,103 @@
+import Nav from '../components/nav'
+
+export default () => (
+  <div style={{marginBottom: '20rem'}}>
+    <Nav />
+    
+    <div className="md:flex">
+      <div className="md:flex-shrink-0">
+        <img className="rounded-lg md:w-56" src='/static/logo.png' alt="Woman paying for a purchase" />
+      </div>
+      <div className="mt-4 md:mt-0 md:ml-6">
+        <div className="uppercase tracking-wide text-sm text-indigo-600 font-bold">Marketing</div>
+        <a href="#" className="block mt-1 text-lg leading-tight font-semibold text-gray-900 hover:underline">Finding customers for your new business</a>
+        <p className="mt-2 text-gray-600">Getting a new business off the ground is a lot of hard work. Here are five ideas you can use to find your first customers.</p>
+      </div>
+    </div>
+
+    <button className="btn btn-blue">
+      Button
+    </button>
+
+    <button className="btn btn-gray">
+      Button
+    </button>
+
+    <div className="bg-gradient-brand">
+      asfffffffffff
+    </div>
+
+    <br />
+   
+    <input placeholder="test" className="border-black focus:bg-white hover:bg-black focus:border-white hover:border-black" />
+  
+    <ul className="list-disc list-inside">
+      <li>One</li>
+      <li>Two</li>
+      <li>Three</li>
+      <li>Four</li>
+    </ul>
+
+    <div className="bg-gray-200 p-4">
+      <div className="block text-gray-700 text-center bg-gray-400 px-4 py-2">1</div>
+      <div className="block text-gray-700 text-center bg-gray-400 px-4 py-2 mt-4">2</div>
+      <div className="block text-gray-700 text-center bg-gray-400 px-4 py-2 mt-4">3</div>
+    </div>
+
+    <div className="bg-gray-200 m-5">
+      <div className="inline-block text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">1</div>
+      <div className="inline-block text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">2</div>
+      <div className="inline-block text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">3</div>
+    </div>
+
+    <div className="bg-gray-200 m-5">
+      <div className="inline text-gray-700 text-center bg-gray-400 px-4 py-2 mx-2">1</div>
+      <div className="inline text-gray-700 text-center bg-gray-400 px-4 py-2 mx-2">2</div>
+      <div className="inline text-gray-700 text-center bg-gray-400 px-4 py-2 mx-2">3</div>
+    </div>
+
+    <div className="flex bg-gray-200 m-5">
+      <div className="flex-1 text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">1</div>
+      <div className="flex-1 text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">2</div>
+      <div className="flex-1 text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">3</div>
+    </div>
+
+    <div className="inline-flex bg-gray-200 m-5">
+      <div className="flex-1 text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">1</div>
+      <div className="flex-1 text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">2</div>
+      <div className="flex-1 text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">3</div>
+    </div>
+
+    <div className="table w-full">
+      <div className="table-row">
+        <div className="table-cell bg-gray-400 text-gray-700 px-4 py-2 text-sm">A cell with more content</div>
+        <div className="table-cell bg-gray-200 text-gray-700 px-4 py-2 text-sm">Cell 2</div>
+        <div className="table-cell bg-gray-400 text-gray-700 px-4 py-2 text-sm">Cell 3</div>
+      </div>
+
+      <div className="table-row">
+        <div className="table-cell bg-gray-200 text-gray-700 px-4 py-2 text-sm">Cell 4</div>
+        <div className="table-cell bg-gray-400 text-gray-700 px-4 py-2 text-sm">A cell with more content</div>
+        <div className="table-cell bg-gray-200 text-gray-700 px-4 py-2 text-sm">Cell 6</div>
+      </div>
+    </div>
+
+    <div className="flex sm:inline-block md:block lg:hidden sl:flex bg-gray-200 m-5">
+      <div className="flex-1 text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">1</div>
+      <div className="flex-1 text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">2</div>
+      <div className="flex-1 text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">3</div>
+    </div>
+
+    {/* <div className="bg-gray-200 p-4 clearfix">
+      <img className="float-left mr-4 my-2 h-32" src="https://images.unsplash.com/photo-1459262838948-3e2de6c1ec80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80" />
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam venenatis et lorem sit amet vehicula
+      </p>
+    </div>
+
+    <div className="bg-gray-400">
+      <img className="object-scale-down h-48 w-full ..." src="https://images.unsplash.com/photo-1459262838948-3e2de6c1ec80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80" />
+    </div>
+     */}
+  </div>
+)
